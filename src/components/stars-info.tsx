5f8f4c7213416b86918269449e8b0da61069adbc
Tryncha <juanes.aguirre26@gmail.com>
2026-04-19 22:31:37 -0500

    cursor changed and debug inputs added

--- src/components/stars-info.tsx
+++ src/components/stars-info.tsx
@@ -1,22 +1,46 @@
 'use client';
 
+import { useState } from 'react';
 import { useGacha } from '../context/gacha-context';
 import { kiwiSoda } from '../lib/fonts';
 
 const StarsInfo = () => {
   const { stars, addStars } = useGacha();
 
+  const [bgColor, setBgColor] = useState('#e7f0ac');
+  const [textColor, setTextColor] = useState('#b8555b');
+
   return (
     <div className="absolute top-2 right-2 z-10 flex w-28 flex-col gap-2">
-      <span className={`${kiwiSoda.className} rounded-md bg-[#e7f0ac] p-2 pb-1 text-center text-5xl text-[#b8555b]`}>
+      <span
+        style={{ backgroundColor: bgColor, color: textColor }}
+        className={`${kiwiSoda.className} rounded-md p-2 pb-1 text-center text-5xl`}
+      >
         {stars}
       </span>
       <button
         onClick={() => addStars(70)}
-        className={`${kiwiSoda.className} flex items-center justify-center rounded-md bg-[#e7f0ac] px-2 text-xl text-[#b8555b] transition-colors hover:cursor-pointer hover:bg-[#ecf89c]`}
+        style={{ backgroundColor: bgColor, color: textColor }}
+        className={`${kiwiSoda.className} flex items-center justify-center rounded-md px-2 text-xl transition-colors hover:cursor-pointer`}
       >
         Add stars
       </button>
+      <div className="flex flex-col bg-white p-2 text-xs">
+        <span>Fondo: {bgColor}</span>
+        <span>Texto: {textColor}</span>
+      </div>
+      <div className="flex justify-between gap-1">
+        <input
+          type="color"
+          value={bgColor}
+          onChange={(e) => setBgColor(e.target.value)}
+        />
+        <input
+          type="color"
+          value={textColor}
+          onChange={(e) => setTextColor(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
